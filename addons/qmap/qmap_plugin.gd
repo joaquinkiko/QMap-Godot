cf57c78bbfb80c952783544f533f42f0c5657c37
Joaquin C <joaquinfcuellar@gmail.com>
2025-09-18 11:53:32 -0600

    registered .lmp saver/loader in plugin

--- addons/qmap/qmap_plugin.gd
+++ addons/qmap/qmap_plugin.gd
@@ -7,6 +7,8 @@ var wad_loader: ResourceFormatLoader
 var wad_saver: ResourceFormatSaver
 var qmap_loader: ResourceFormatLoader
 var qmap_saver: ResourceFormatSaver
+var lmp_loader: ResourceFormatLoader
+var lmp_saver: ResourceFormatSaver
 
 func _enter_tree() -> void:
 	fgd_loader = FGDResourceLoader.new()
@@ -15,12 +17,16 @@ func _enter_tree() -> void:
 	wad_saver = WADResourceSaver.new()
 	qmap_loader = QMapResourceLoader.new()
 	qmap_saver = QMapResourceSaver.new()
+	lmp_loader = QPaletteResourceLoader.new()
+	lmp_saver = QPaletteResourceSaver.new()
 	ResourceLoader.add_resource_format_loader(fgd_loader)
 	ResourceSaver.add_resource_format_saver(fgd_saver)
 	ResourceLoader.add_resource_format_loader(wad_loader)
 	ResourceSaver.add_resource_format_saver(wad_saver)
 	ResourceLoader.add_resource_format_loader(qmap_loader)
 	ResourceSaver.add_resource_format_saver(qmap_saver)
+	ResourceLoader.add_resource_format_loader(lmp_loader)
+	ResourceSaver.add_resource_format_saver(lmp_saver)
 
 func _exit_tree() -> void:
 	ResourceLoader.remove_resource_format_loader(fgd_loader)
@@ -29,3 +35,5 @@ func _exit_tree() -> void:
 	ResourceSaver.remove_resource_format_saver(wad_saver)
 	ResourceLoader.remove_resource_format_loader(qmap_loader)
 	ResourceSaver.remove_resource_format_saver(qmap_saver)
+	ResourceLoader.remove_resource_format_loader(lmp_loader)
+	ResourceSaver.remove_resource_format_saver(lmp_saver)
