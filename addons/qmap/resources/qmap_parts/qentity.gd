a9077eba103094cca63fbb9b51baf4e848ffcf0c
Joaquin C <joaquinfcuellar@gmail.com>
2025-09-18 09:18:52 -0600

    cleaned up deprecated
    Removed deprecated vars and functions and updated comments

--- addons/qmap/resources/qmap_parts/qentity.gd
+++ addons/qmap/resources/qmap_parts/qentity.gd
@@ -1,7 +1,9 @@
 ## Represent a [QMap] entity for parsing
 class_name QEntity extends Resource
 
-var comments: String
+## Classname to find in [FGD]
 @export var classname: String
+## Property key values to apply when spawned
 @export var properties: Dictionary[String, String]
+## Solid geometry
 @export var brushes: Array[QBrush]
