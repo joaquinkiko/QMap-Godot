738a6925e4259c4b956ac1c1a904cfaa5ddc2263
Joaquin C <joaquinfcuellar@gmail.com>
2025-11-07 10:58:34 -0700

    Update qmap_plugin.gd

--- addons/qmap/qmap_plugin.gd
+++ addons/qmap/qmap_plugin.gd
@@ -5,27 +5,18 @@ const PROP_AUTOSAVE_DIR := &"qmap/ignore_map_autosave_dir"
 const PROP_TRENCHBROOM_DIR := &"qmap/trenchbroom/games_config_dir"
 const PROP_TRENCHBROOM_VERSION := &"qmap/trenchbroom/config_version"
 
-var fgd_loader: ResourceFormatLoader
-var fgd_saver: ResourceFormatSaver
-var wad_loader: ResourceFormatLoader
-var wad_saver: ResourceFormatSaver
-var qmap_loader: ResourceFormatLoader
-var qmap_saver: ResourceFormatSaver
-var lmp_loader: ResourceFormatLoader
-var lmp_saver: ResourceFormatSaver
+var fgd_loader := FGDResourceLoader.new()
+var fgd_saver := FGDResourceSaver.new()
+var wad_loader := WADResourceLoader.new()
+var wad_saver := WADResourceSaver.new()
+var qmap_loader := QMapResourceLoader.new()
+var qmap_saver := QMapResourceSaver.new()
+var lmp_loader := QPaletteResourceLoader.new()
+var lmp_saver := QPaletteResourceSaver.new()
 
-var local_settings: EditorSettings
+var local_settings := EditorInterface.get_editor_settings()
 
 func _enter_tree() -> void:
-	local_settings = EditorInterface.get_editor_settings()
-	fgd_loader = FGDResourceLoader.new()
-	fgd_saver = FGDResourceSaver.new()
-	wad_loader = WADResourceLoader.new()
-	wad_saver = WADResourceSaver.new()
-	qmap_loader = QMapResourceLoader.new()
-	qmap_saver = QMapResourceSaver.new()
-	lmp_loader = QPaletteResourceLoader.new()
-	lmp_saver = QPaletteResourceSaver.new()
 	ResourceLoader.add_resource_format_loader(fgd_loader)
 	ResourceSaver.add_resource_format_saver(fgd_saver)
 	ResourceLoader.add_resource_format_loader(wad_loader)
@@ -34,30 +25,6 @@ func _enter_tree() -> void:
 	ResourceSaver.add_resource_format_saver(qmap_saver)
 	ResourceLoader.add_resource_format_loader(lmp_loader)
 	ResourceSaver.add_resource_format_saver(lmp_saver)
-	local_settings.set_setting(PROP_AUTOSAVE_DIR, true)
-	local_settings.set_initial_value(PROP_AUTOSAVE_DIR, true, false)
-	local_settings.add_property_info({
-		"name": PROP_AUTOSAVE_DIR,
-		"type": TYPE_BOOL,
-		"hint": PROPERTY_HINT_NONE,
-		"hint_string": "When enabled will generate .gdignore in 'autosave' directory relative to map files"
-	})
-	local_settings.set_setting(PROP_TRENCHBROOM_DIR, "")
-	local_settings.set_initial_value(PROP_TRENCHBROOM_DIR, "", false)
-	local_settings.add_property_info({
-		"name": PROP_TRENCHBROOM_DIR,
-		"type": TYPE_STRING,
-		"hint": PROPERTY_HINT_GLOBAL_DIR,
-		"hint_string": "Path to Trenchbroom games directory"
-	})
-	local_settings.set_setting(PROP_TRENCHBROOM_VERSION, 0)
-	local_settings.set_initial_value(PROP_TRENCHBROOM_VERSION, 0, false)
-	local_settings.add_property_info({
-		"name": PROP_TRENCHBROOM_VERSION,
-		"type": TYPE_INT,
-		"hint": PROPERTY_HINT_ENUM,
-		"hint_string": "Latest:0,Version 4:4,Version 8:8,Version 9:9"
-	})
 
 func _exit_tree() -> void:
 	ResourceLoader.remove_resource_format_loader(fgd_loader)
@@ -68,3 +35,37 @@ func _exit_tree() -> void:
 	ResourceSaver.remove_resource_format_saver(qmap_saver)
 	ResourceLoader.remove_resource_format_loader(lmp_loader)
 	ResourceSaver.remove_resource_format_saver(lmp_saver)
+
+func _enable_plugin() -> void:
+	if !local_settings.has_setting(PROP_AUTOSAVE_DIR):
+		local_settings.set_setting(PROP_AUTOSAVE_DIR, true)
+	local_settings.set_initial_value(PROP_AUTOSAVE_DIR, true, false)
+	local_settings.add_property_info({
+		"name": PROP_AUTOSAVE_DIR,
+		"type": TYPE_BOOL,
+		"hint": PROPERTY_HINT_NONE,
+		"hint_string": "When enabled will generate .gdignore in 'autosave' directory relative to map files"
+	})
+	if !local_settings.has_setting(PROP_TRENCHBROOM_DIR):
+		local_settings.set_setting(PROP_TRENCHBROOM_DIR, "")
+	local_settings.set_initial_value(PROP_TRENCHBROOM_DIR, "", false)
+	local_settings.add_property_info({
+		"name": PROP_TRENCHBROOM_DIR,
+		"type": TYPE_STRING,
+		"hint": PROPERTY_HINT_GLOBAL_DIR,
+		"hint_string": "Path to Trenchbroom games directory"
+	})
+	if !local_settings.has_setting(PROP_TRENCHBROOM_VERSION):
+		local_settings.set_setting(PROP_TRENCHBROOM_VERSION, 0)
+	local_settings.set_initial_value(PROP_TRENCHBROOM_VERSION, 0, false)
+	local_settings.add_property_info({
+		"name": PROP_TRENCHBROOM_VERSION,
+		"type": TYPE_INT,
+		"hint": PROPERTY_HINT_ENUM,
+		"hint_string": "Latest:0,Version 4:4,Version 8:8,Version 9:9"
+	})
+
+func _disable_plugin() -> void:
+	local_settings.erase(PROP_AUTOSAVE_DIR)
+	local_settings.erase(PROP_TRENCHBROOM_DIR)
+	local_settings.erase(PROP_TRENCHBROOM_VERSION)
